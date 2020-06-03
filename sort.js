b36390ee1c40e5d86a5e33b24e4f0b7fed5089bb
KAZ <devidevi.man2011@gmail.com>
2020-06-03 16:30:32 +0900

    conf2

--- sort.js
+++ sort.js
@@ -8,7 +8,7 @@ var sortNumber = function (number) {
    });
 };
 
-var number = [999];
+var number = [999, 1, 2, 3];
 var number2 = [19, 3, 81, 1, 24, 21, 999];
 sortNumber(number);
 console.log(number);
